c8903126303772eb473fdbef70dd80035d405047
JJH <skkuwjswnsgur456@gmail.com>
2024-01-08 23:35:28 +0900

    try to use sele re

--- wikidocs/wikidocs.py
+++ wikidocs/wikidocs.py
@@ -1,1 +1,16 @@
-print(8//2*(2+2))
+from selenium import webdriver
+
+# selenium으로 키를 조작하기 위한 import
+from selenium.webdriver.common.keys import Keys
+
+# 페이지 로딩을 기다리는데에 사용할 time 모듈 import
+import time
+
+# 크롬드라이버 실행
+driver = webdriver.Chrome() 
+
+#크롬 드라이버에 url 주소 넣고 실행
+driver.get('https://www.google.co.kr/')
+
+# 페이지가 완전히 로딩되도록 3초동안 기다림
+time.sleep(3)
