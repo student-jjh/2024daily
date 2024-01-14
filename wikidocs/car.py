4ca6c0bbae74be913471dc7c2fc4e2a6283ac347
JJH <skkuwjswnsgur456@gmail.com>
2024-01-14 19:47:01 +0900

    real end

---
+++ wikidocs/car.py
@@ -0,0 +1,13 @@
+class car:
+
+    def __init__(self,wheel,price) -> None:
+        self.wheel = wheel
+        self.price = price
+
+
+class carbike(car):
+    def __init__(self, 바퀴, 가격,구동계):
+        self.바퀴 = 바퀴
+        self.가격 = 가격
+        self.구동계 = 구동계
+        
