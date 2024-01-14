d1fdeed7a8a3ab6b6c261cc0148a4853864b7d18
JJH <skkuwjswnsgur456@gmail.com>
2024-01-14 19:32:06 +0900

    end of code study

--- wikidocs/account.py
+++ wikidocs/account.py
@@ -19,13 +19,37 @@ class Account:
         self.account_number = num1 + '-' + num2 + '-' + num3  # 001-01-000001
 
         Account.account_count +=1
+ 
+    def deposit(self,num):
+        if num >=1:
+            self.balance+=num
+    
+    def withdraw(self,num):
+        if num <= self.balance:
+            self.balance-=num
 
+    def display_info(self):
+        print("은행이름: ", self.bank)
+        print("예금주: ", self.name)
+        print("계좌번호: ", self.account_number)
+        print("잔고: ", f"{self.balance:,}")
+
+
+    @classmethod
+    def get_account_num(cls):
+        print(cls.account_count)
 
 kim = Account("김민수", 100)
 lee = Account("리민수", 100)
+
+
+lee.deposit(1000000000)
+lee.display_info()
 # print(kim.name)
 # print(kim.balance)
 # print(kim.bank)
 # print(kim.account_number)
 
-print(Account.account_count)
+# print(Account.account_count)
+
+kim.get_account_num()
